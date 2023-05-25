851585604c066518d38deb79f81c0c1ec2689499
Evan Grissino <evanjgrissino@gmail.com>
2023-05-25 13:03:40 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum install -y git python python-pip
+sudo yum install -y git python3 python3-pip
 #
 # Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
@@ -8,7 +8,6 @@ sudo dnf -y localinstall ./google-chrome-stable_current_x86_64.rpm
 #
 wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-
 #
 # install ffmpeg
 sudo mkdir /usr/local/ffmpeg
@@ -17,12 +16,10 @@ sudo tar -xf /usr/local/ffmpeg/ffmpeg-git-amd64-static.tar.xz --output /usr/loca
 sudo ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
 #
 # Download whisper yt AIJ
-cd /~
 git clone https://github.com/egrissino/whisper-youtube.git
 #
 # Install ppython dependencies
-pip install git+https://github.com/openai/whisper.git yt-dlp
-pip install yt-dlp
-pip install -r ./whisper-youtube/requirements.txt
+pip3 install git+https://github.com/openai/whisper.git yt-dlp
+pip3 install -r ./whisper-youtube/requirements.txt
 #
 #
