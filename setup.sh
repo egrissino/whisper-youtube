5133323c0028ed71940559d9e8e59d27a3b51adc
Evan Grissino <evanjgrissino@gmail.com>
2023-05-25 10:55:28 -0700

    Create setup.sh
    setup for AWS server

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo yum update
+sudo yum install git pyhton python-pip
+#
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf localinstall ./google-chrome-stable_current_x86_64.rpm
+#
+wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+
+#
+# install ffmpeg
+sudo mkdir /usr/local/ffmpeg
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+sudo tar -xf /usr/local/ffmpeg/ffmpeg-git-amd64-static.tar.xz --output /usr/local/ffmpeg
+sudo ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+#
+# Download whisper yt AIJ
+cd /~
+git clone https://github.com/egrissino/whisper-youtube.git
+#
+# Install ppython dependencies
+pip install git+https://github.com/openai/whisper.git yt-dlp
+pip install yt-dlp
+pip install -r ~/whisper-youtube/requirements.txt
+#
+#
