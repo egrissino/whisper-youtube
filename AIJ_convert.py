8d05bdcb403124e5f038f27f21bc353801cb34c6
Evan Grissino <evanjgrissino@gmail.com>
2023-05-26 16:02:07 -0700

    Create AIJ_convert.py

---
+++ AIJ_convert.py
@@ -0,0 +1,29 @@
+'''
+
+
+AIJ Transcript Name Converter
+
+
+'''
+
+import os
+import shutil
+import SeleniumYT as syt
+import AIJ_transcript as aijt
+
+if __name__ == "__main__":
+
+    argc = len(sys.argv)
+    if argc > 1:
+        out_dir = sys.argv[2]
+
+    print("Running in directory: " + out_dir)
+
+    links = aijt.getLinks()
+
+    # Rename to video name
+    filename = out_dir + syt.getFilenameFromURL(url)
+    newFilename = out_dir + links[url][2] + ".txt"
+
+    if os.path.exists(filename):
+        shutil.copy(filename, newFilename)
