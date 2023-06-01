c44a2d4ab8e7a585f0d0361f10e98c0c7a344274
Evan Grissino <evang62@gmail.com>
2023-06-01 16:44:07 -0700

    Update setup.sh
    Fixes and cross platform compatibilty

--- setup.sh
+++ setup.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y git python3 python3-pip
 #
-# Install Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo dnf -y localinstall ./google-chrome-stable_current_x86_64.rpm
+export RM=""
+if type "yum" > /dev/null 2>&1; then
+  export RM=yum
+elif type "apt" > /dev/null 2>&1; then
+  export RM=apt
+elif type "dnf" > /dev/null 2>&1; then
+  export RM=ydnf
+fi
 #
-wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
+if [[ $RM != "" ]]; then
+  # update and install deps
+  sudo $RM update
+  sudo $RM install -y git python3 python3-pip unzip cuda-toolkit-12-1
+  #
+  # Install Chrome
+  if ! type "google-chrome" > /dev/null 2>&1; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    sudo $RM -y localinstall ./google-chrome-stable_current_x86_64.rpm
+  fi
+  #
+fi
+#
+if ! type "chromedriver" > /dev/null 2>&1; then
+  wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
+  unzip chromedriver_linux64.zip
+fi
 #
 # install ffmpeg
-sudo mkdir /usr/local/ffmpeg
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-sudo tar -xf /usr/local/ffmpeg/ffmpeg-git-amd64-static.tar.xz --output /usr/local/ffmpeg
-sudo ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+if ! type "ffmpeg" > /dev/null 2>&1; then
+  sudo mkdir /usr/local/bin/ffmpeg/
+  wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+  sudo tar -xf ./ffmpeg-git-amd64-static.tar.xz
+  sudo cp -r ./ffmpeg-git-20230313-amd64-static/* /usr/local/bin/ffmpeg/
+  sudo ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+fi
 #
 # Download whisper yt AIJ
 git clone https://github.com/egrissino/whisper-youtube.git
